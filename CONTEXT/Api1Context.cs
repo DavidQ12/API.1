108f36489bb252530f3eacd858ba1634bdf25b2d
Computo <Computo@LABC1PC8>
2022-11-30 08:40:00 -0600

    Agregar archivos de proyecto.

---
+++ CONTEXT/Api1Context.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API._1.CONTEXT
+{
+    public class Api1Context : DbContext
+    {
+        public Api1Context(DbContextOptions<Api1Context> options) : base(options)
+        {
+
+    }
+        
+        public DbSet<MODELS.Items> Items { get; set; }
+
+    }
+
+
+
+
+
+
+}
---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API._1.CONTEXT;
+using API._1.MODELS;
+
+namespace API._1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private readonly Api1Context _context;
+
+        public ItemsController(Api1Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Items
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Items>>> GetItems()
+        {
+            return await _context.Items.ToListAsync();
+        }
+
+        // GET: api/Items/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Items>> GetItems(long id)
+        {
+            var items = await _context.Items.FindAsync(id);
+
+            if (items == null)
+            {
+                return NotFound();
+            }
+
+            return items;
+        }
+
+        // PUT: api/Items/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutItems(long id, Items items)
+        {
+            if (id != items.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(items).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ItemsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Items
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Items>> PostItems(Items items)
+        {
+            _context.Items.Add(items);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetItems", new { id = items.Id }, items);
+        }
+
+        // DELETE: api/Items/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Items>> DeleteItems(long id)
+        {
+            var items = await _context.Items.FindAsync(id);
+            if (items == null)
+            {
+                return NotFound();
+            }
+
+            _context.Items.Remove(items);
+            await _context.SaveChangesAsync();
+
+            return items;
+        }
+
+        private bool ItemsExists(long id)
+        {
+            return _context.Items.Any(e => e.Id == id);
+        }
+    }
+}
